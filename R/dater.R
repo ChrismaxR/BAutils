97081e8ed1fd33efa79b953f6519f5f82182038f
Christian van Gaalen <ctmvangaalen@gmail.com>
2022-11-07 16:29:10 +0100

    Added more context for the use of the dater function

--- R/dater.R
+++ R/dater.R
@@ -1,15 +1,6 @@
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+# This is my function named 'dater', it's a shorthand for creating a string of the
+# date, so you can use it in naming an output file (.csv, .xlsx, or whatever)
+# to make it easy to structure output dirs.
 
 #'@param ts A timestamp as produced by Sys.Date() or lubridate::today()
 #'
