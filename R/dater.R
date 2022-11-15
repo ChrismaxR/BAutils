c4b1fbd7d4b273aa688ab0a0952b27920053c4f2
Christian van Gaalen <ctmvangaalen@gmail.com>
2022-11-15 11:43:38 +0100

    small documentation addition

--- R/dater.R
+++ R/dater.R
@@ -1,6 +1,6 @@
 # This is my function named 'dater', it's a shorthand for creating a string of the
 # date, so you can use it in naming an output file (.csv, .xlsx, or whatever)
-# to make it easy to structure output dirs.
+# to make it easy to structure output files and dirs.
 
 #'@param ts A timestamp as produced by Sys.Date() or lubridate::today()
 #'
