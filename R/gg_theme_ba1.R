1e48bd2a1810ea5aa016eddfcda50a6957b96a3d
Christian van Gaalen <ctmvangaalen@gmail.com>
2022-11-17 13:43:28 +0100

    Added other fonts and updated the DESCRIPTION file with dependencies

--- R/gg_theme_ba1.R
+++ R/gg_theme_ba1.R
@@ -1,19 +1,23 @@
+# theme picked up from this source: https://www.cararthompson.com/talks/nhsr2022-ggplot-themes/
+# This theme helps in adding a Text hierarchy, so helping readers orient to the
+# most important parts of the graph, let the graph breath more than the standard
+# ggplot plots
+
 gg_theme_ba1 <- function(base_size = 12) {
 
   dark_text = "#1A242F"
   mid_text <-  monochromeR::generate_palette(dark_text, "go_lighter", n_colours = 5)[2]
   light_text <-  monochromeR::generate_palette(dark_text, "go_lighter", n_colours = 5)[3]
-  fontfamily <- "Roboto Condensed"
 
   theme_minimal(base_size = base_size) +
     theme(
       text = element_text(colour = mid_text, family = fontfamily, lineheight = 1.1),
       plot.title = element_text(
-        colour = dark_text, family = "Roboto Flex",
+        colour = dark_text, family = "BrandonText",
         size = rel(1.6), margin = margin(12, 0, 8, 0)
       ),
       plot.subtitle = element_text(
-        size = rel(1.1), family = "Roboto Condensed",
+        size = rel(1.1), family = "EnriquetaSB",
         margin = margin(4, 0, 0, 0)
       ),
       axis.text.y = element_text(colour = light_text, size = rel(0.8)),
